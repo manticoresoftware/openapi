0fc5c1e0b92fdcc2e83f0f7d735e744f01153c0f
Adrian Nuta <adriannuta@gmail.com>
2020-08-13 16:00:45 +0200

    fix python test host

--- out/manticore-python/test/test.py
+++ out/manticore-python/test/test.py
@@ -6,7 +6,7 @@ import inspect
 from parametrized_test_case import ParametrizedTestCase
 
 configuration = openapi_client.Configuration(
-    host = "manticoresearch-manticore:9308"
+    host = "http://manticoresearch-manticore:9308"
 )
 
 print
