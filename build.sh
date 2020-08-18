e59be4a869c45f703b10a190b4b65a034ab36975
Adrian Nuta <adriannuta@gmail.com>
2020-08-18 15:40:02 +0200

    add back inherits in dev deps

--- build.sh
+++ build.sh
@@ -13,8 +13,8 @@ do_python() {
 
 do_java() {
   echo "Building Java ..."
-  rm out/manticore-java -rf
-  docker run --rm -v ${PWD}:/local  -u "$(id -u):$(id -g)"  -e JAVA_OPTS="-Dlog.level=warn" openapitools/openapi-generator-cli generate -i /local/manticore.yml -g java  -o /local/out/manticore-java -t /local/templates/Java --git-repo-id manticoresearch-java --git-user-id manticoresoftware
+  rm out/manticoresearch-java -rf
+  docker run --rm -v ${PWD}:/local  -u "$(id -u):$(id -g)"  -e JAVA_OPTS="-Dlog.level=warn" openapitools/openapi-generator-cli generate -i /local/manticore.yml -g java  -o /local/out/manticoresearch-java -t /local/templates/Java --git-repo-id manticoresearch-java --git-user-id manticoresoftware
   git apply patches/java.patch
   echo "Java done."
 }
