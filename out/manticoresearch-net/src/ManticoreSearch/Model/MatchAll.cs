e3883a7950ec7fee2a10e43019d6be598d52c997
Nick <nick@manticoresearch.com>
2025-06-30 14:51:39 +0700

    Regenerated the clients

--- out/manticoresearch-net/src/ManticoreSearch/Model/MatchAll.cs
+++ out/manticoresearch-net/src/ManticoreSearch/Model/MatchAll.cs
@@ -43,7 +43,7 @@ namespace ManticoreSearch.Model
             /// Enum  for value: {}
             /// </summary>
             [EnumMember(Value = "{}")]
-             = 1
+            All = 1
         }
 
 
@@ -51,7 +51,7 @@ namespace ManticoreSearch.Model
         /// Gets or Sets All
         /// </summary>
         [DataMember(Name = "_all", IsRequired = true, EmitDefaultValue = true)]
-        public AllEnum All { get; set; }
+        public String All { get; set; }
         /// <summary>
         /// Initializes a new instance of the <see cref="MatchAll" /> class.
         /// </summary>
@@ -60,10 +60,10 @@ namespace ManticoreSearch.Model
         /// <summary>
         /// Initializes a new instance of the <see cref="MatchAll" /> class.
         /// </summary>
-        /// <param name="All"> (required).</param>
-        public MatchAll(AllEnum  = default(AllEnum))
+        /// <param name="All">All (required).</param>
+        public MatchAll(AllEnum All = default(AllEnum))
         {
-            this.All = ;
+            this.All = "{}";
         }
 
         /// <summary>
